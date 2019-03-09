7da2162aac994570435a5b3be187a7a829643f32
Your Name <you@example.com>
2019-03-09 23:49:10 +0800

    配置webpack

---
+++ client/app.js
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import { AppContainer } from 'react-hot-loader'
+import App from './App.jsx'
+
+// ReactDOM.hydrate(<App />, document.getElementById('app'))
+
+const root = document.getElementById('app')
+const render = Component => {
+  ReactDOM.hydrate(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App.jsx', () => {
+    const NextApp = require('./App.jsx').default
+    // ReactDOM.hydrate(<NextApp />, document.getElementById('app'))
+    render(NextApp)
+  })
+}
